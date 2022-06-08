dd63966b341120d28b704d120443fd4d1b3b133c
Kaitlyn Greve <kgreve14@gmail.com>
2022-06-08 11:30:53 -0400

    commit tree 00bf5e66905e38db6003c865413e9d386cb21bc6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+t1 = Taxi.create(title: "Taxi One")
+t2 = Taxi.create(title: "Taxi Two")
+
+p1 = Passenger.create(name: "Joe")
+p2 = Passenger.create(name: "Nat")
+
+Ride.create(taxi_id: t1.id, passenger_id: p1.id)
+Ride.create(taxi_id: t2.id, passenger_id: p2.id)
+
+puts "done seeding"
